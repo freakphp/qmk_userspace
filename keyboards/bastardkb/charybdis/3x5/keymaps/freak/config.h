85db785c4c2934ff9ff75d850689886be959fac8
freakphp <mail@freak.io>
2026-03-05 14:28:27 +0100

    Add freak keymap and change key matrix for svlinky

---
+++ keyboards/bastardkb/charybdis/3x5/keymaps/freak/config.h
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+/* -------------------------------------------------
+ * Svlinky v0.2 matrix pin translation for Charybdis
+ * (Splinky shield wiring, but on Svlinky v0.2)
+ *
+ * Splinky typical:
+ *   Cols: GP27, GP28, GP21, GP6, GP7, GP8
+ *   Rows: GP29, GP26, GP5, GP4, GP9
+ *
+ * Svlinky v0.2 swap:
+ *   GP27 -> GP18
+ *   GP26 -> GP24
+ * ------------------------------------------------- */
+#undef MATRIX_COL_PINS
+#undef MATRIX_ROW_PINS
+#define MATRIX_COL_PINS { GP18, GP28, GP21, GP6, GP7, GP8 }
+#define MATRIX_ROW_PINS { GP29, GP24, GP5, GP4, GP9 }
+
+#ifdef VIA_ENABLE
+/* VIA configuration. */
+#    define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#endif // VIA_ENABLE
+
+#ifndef __arm__
+/* Disable unused features. */
+#    define NO_ACTION_ONESHOT
+#endif // __arm__
+
+/* Charybdis-specific features. */
+
+#ifdef POINTING_DEVICE_ENABLE
+// Automatically enable the pointer layer when moving the trackball.  See also:
+// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS`
+// - `CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD`
+// #define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+#endif // POINTING_DEVICE_ENABLE
